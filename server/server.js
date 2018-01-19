3d22f77a6af49df0184d9ea6f9fbc0ecbe5c583e
natecarson94 <33134997+natecarson94@users.noreply.github.com>
2018-01-19 13:53:48 -0500

    greet new users and alert others

--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection',(socket) =>{
     console.log('New user connected');
 
+socket.emit('newMessage',{
+    from: 'admin',
+    text: 'Welcome to the chat app!'
+});
+
+socket.broadcast.emit('newMessage',{
+    from:'Admin',
+    text: 'New User Joined'
+});
+
     socket.on('disconnect', ()=>{
         console.log('Client Disconnected');
     });
@@ -21,7 +31,16 @@ io.on('connection',(socket) =>{
 
     socket.on('createMessage',(message)=>{
         console.log('createMessage',message);
-        io.emit('newMessage',{from: message.from, text: message.text, createdAt: new Date().getTime()})
+        io.emit('newMessage',{
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
+        // socket.broadcast.emit('newMessage',{
+        //     from: message.from,
+        //     text: message.text,
+        //     createAt: new Date().getTime()
+        // });
     });
 
 
